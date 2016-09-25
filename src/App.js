a1fe2a88ab1691568250068215ab74126708d575
Willy Yang <willyyang-@Willys-MacBook-Pro.local>
2016-09-25 18:39:58 -0400

    Save local changes

--- src/App.js
+++ src/App.js
@@ -35,15 +35,21 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <div className="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
-           <a className="pure-menu-heading" href="">Welcome, My Friend!</a>
-           <ul >
-              <li onClick={() => this._changeBodyContent('aboutMe')} ><a href="#">About Me</a></li>
-              <li onClick={() => this._changeBodyContent('projects')} ><a href="#" >Projects</a></li>
-              <li onClick={() => this._changeBodyContent('blogs')}><a href="#" >Blog(s)</a></li>
+        <div className="header-home-menu">
+          <div className="header-div-2-left">
+            <a className="pure-menu-heading" href="">Welcome, My Friend!</a>
+          </div>
+          <div className="header-div-2-right">
+            <ul className="haeder-ul">
+              <li onClick={() => this._changeBodyContent('aboutMe')}><a href="#" >About Me</a></li>
+              <li onClick={() => this._changeBodyContent('projects')}><a href="#">Projects</a></li>
+              <li onClick={() => this._changeBodyContent('blogs')}><a href="#">Blog(s)</a></li>
               <li onClick={() => this._changeBodyContent('fitness')}><a href="#">Fitness</a></li>
             </ul>
+          </div>
+          
         </div>
+       
         {this._getBodyContent()}
       </div>
     );
